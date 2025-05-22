df05702f390f07849bf358f4bc80be3c89cdbcdc
Uzain Bin Mubarak <uzainmubarak@gmail.com>
2025-05-23 00:59:42 +0500

    refactor(db): remove commented-out code and unused imports
    Clean up the db.py file by removing commented-out code and unused imports to improve readability and maintainability. The changes do not affect the functionality of the code.

--- app/db.py
+++ app/db.py
@@ -1,18 +1,6 @@
 from sqlmodel import SQLModel, create_engine
-# import os
-# from dotenv import load_dotenv, find_dotenv
 
 from app.config import settings
-
-
-# load_dotenv()
-# DATABASE_URL = os.environ.get("DB_LINK")
-
-# ENVIRONMENT = os.environ.get("ENVIRONMENT")
-# if ENVIRONMENT == "DEVELOPMENT":
-#     DATABASE_URL = os.environ.get("DB_LINK")
-
-print(settings.db_url)
 
 engine = create_engine(settings.db_url, echo=True)
 def create_db_and_tables():
